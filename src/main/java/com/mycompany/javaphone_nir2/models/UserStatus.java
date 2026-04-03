549498a498692b1913e48a1b8700a872f4c5810a
AndreyKuzmaDev <91907433+AndreyKuzmaDev@users.noreply.github.com>
2026-04-03 20:24:00 +0300

    feat(models): add enum to represent user status
    Signed-off-by: AndreyKuzmaDev <91907433+AndreyKuzmaDev@users.noreply.github.com>

---
+++ src/main/java/com/mycompany/javaphone_nir2/models/UserStatus.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.javaphone_nir2.models;
+
+public enum UserStatus {
+    ONLINE,
+    OFFLINE,
+    SLEEPING,
+    DO_NOT_DISTURB,
+}
