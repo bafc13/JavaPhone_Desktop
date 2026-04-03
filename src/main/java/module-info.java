92460bd28256aadf75a04b6cc896b268018215ce
AndreyKuzmaDev <91907433+AndreyKuzmaDev@users.noreply.github.com>
2026-04-03 16:31:51 +0300

    chore(deps): add dependencies for signaling and general webrtc usage
    Signed-off-by: AndreyKuzmaDev <91907433+AndreyKuzmaDev@users.noreply.github.com>

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,9 @@ module com.mycompany.javaphone_nir2 {
     requires javafx.fxml;
     requires java.sql;
     requires java.base;
+    requires com.fasterxml.jackson.databind;
+    requires jakarta.websocket;
+    requires webrtc.java;
     
     opens com.mycompany.javaphone_nir2 to javafx.fxml;
     exports com.mycompany.javaphone_nir2;
