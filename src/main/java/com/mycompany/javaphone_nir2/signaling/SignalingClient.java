1ad9722b20ff74ca97e7f9df0159f8a9cb173671
AndreyKuzmaDev <91907433+AndreyKuzmaDev@users.noreply.github.com>
2026-04-06 18:46:54 +0300

    feat(signaling): improve signaling protocol
    Add proper peer handling, split answer message type to accept and reject
    
    Signed-off-by: AndreyKuzmaDev <91907433+AndreyKuzmaDev@users.noreply.github.com>

--- src/main/java/com/mycompany/javaphone_nir2/signaling/SignalingClient.java
+++ src/main/java/com/mycompany/javaphone_nir2/signaling/SignalingClient.java
@@ -13,6 +13,7 @@ import jakarta.websocket.*;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.beans.property.ObjectProperty;
@@ -28,23 +29,37 @@ public class SignalingClient {
     private final ObjectMapper mapper = new ObjectMapper();
 
     private final ObjectProperty<Offer> offer = new SimpleObjectProperty<>();
+    
+    private static SignalingClient instance = null;
+    
+    public static void initialize(String serverUrl) {
+        instance = new SignalingClient(serverUrl);
+    }
+    
+    public static SignalingClient getInstance() {
+        return instance;
+    }
+
+    
     public ObjectProperty<Offer> offerProperty() {
         return offer;
     }
-
+    
     public SignalingClient(String serverUrl) {
         this.serverUrl = serverUrl;
     }
 
-    public void connect() throws Exception {
+    public final void connect() throws Exception {
         WebSocketContainer container = ContainerProvider.getWebSocketContainer();
         container.setDefaultMaxSessionIdleTimeout(2000000);
         container.connectToServer(this, new URI(serverUrl));
+        System.out.println("connected!");
     }
 
     @OnOpen
     public void onOpen(Session session) {
         this.session = session;
+        System.out.println("OPENED SESSION");
     }
 
     @OnMessage
@@ -62,8 +77,11 @@ public class SignalingClient {
             case "offer":
                 handleOffer(json);
                 break;
-            case "answer":
-                handleAnswer(json);
+            case "accept":
+                handleAccept(json);
+                break;
+            case "reject":
+                handleReject(json);
                 break;
             case "candidate":
                 handleCandidate(json);
@@ -79,12 +97,12 @@ public class SignalingClient {
 
     @OnClose
     public void onClose(Session session, CloseReason closeReason) {
-        // logic on call end
+        System.out.println("CLOSED CONNECTION TO SIGNALING SERVER");
     }
 
     @OnError
     public void onError(Session session, Throwable throwable) {
-        // logic on error
+        System.out.println("ERROR IN SIGNALING");
     }
 
     public void sendPeer(User user, UserStatus status) throws IOException {
@@ -94,21 +112,26 @@ public class SignalingClient {
         userNode.put("status", status.toString());
 
         ObjectNode message = mapper.createObjectNode();
+        message.put("type", "peer");
         message.set("user", userNode);
 
         message.put("target", "all");
         message.put("sender", clientId);
 
         sendJson(message);
-
+        System.out.println("SENT PEER WITH TYPE");
         // send to all
     }
 
     private void handlePeer(JsonNode json) {
         try {
-            JsonNode userNode = json.get("user");
+            JsonNode userNode = json.get("user");           
             User user = mapper.treeToValue(userNode.get("data"), User.class);
             UserStatus status = mapper.treeToValue(userNode.get("status"), UserStatus.class);
+            
+            String toPrint = userNode.asText();
+            System.out.println("GOT PEER");
+            System.out.println(toPrint);
             // call UI to hanlde peer
         } catch (IllegalArgumentException ex) {
             Logger.getLogger(SignalingClient.class.getName()).log(Level.SEVERE, null, ex);
@@ -118,6 +141,21 @@ public class SignalingClient {
     }
 
     private void handleWelcome(JsonNode json) {
+        // Get actual user info
+        System.out.println("GOT WELCOME!");
+        
+        User me = new User();
+        me.setName("me");
+        me.setEmail("me@me.me");
+        me.setIp("localhost");
+        me.setPublicKey("123");
+        me.setAvatarId(Integer.MAX_VALUE);
+
+        try {
+            sendPeer(me, UserStatus.ONLINE);
+        } catch (IOException ex) {
+            System.getLogger(SignalingClient.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        }
         // logic on connected
     }
 
@@ -129,9 +167,14 @@ public class SignalingClient {
         this.offer.set(new Offer(sdp, sender));
     }
 
-    private void handleAnswer(JsonNode json) {
+    private void handleAccept(JsonNode json) {
         String sdp = json.get("sdp").asText();
-//        tell webrtc manager to handle answer
+//        tell webrtc manager to handle accept
+    }
+    
+    private void handleReject(JsonNode json) {
+        String sdp = json.get("sdp").asText();
+//        tell webrtc manager to handle reject
     }
 
     private void handleCandidate(JsonNode json) {
@@ -154,8 +197,12 @@ public class SignalingClient {
         sendMessage("offer", sdp, targetClientId);
     }
 
-    public void sendAnswer(String sdp, String targetClientId) throws IOException {
-        sendMessage("answer", sdp, targetClientId);
+    public void sendAccept(String sdp, String targetClientId) throws IOException {
+        sendMessage("accept", sdp, targetClientId);
+    }
+    
+    public void sendReject(String sdp, String targetClientId) throws IOException {
+        sendMessage("reject", sdp, targetClientId);
     }
 
     public void sendIceCandidate(String candidate, String sdpMid, int sdpMLineIndex, String targetClientId) throws IOException {
